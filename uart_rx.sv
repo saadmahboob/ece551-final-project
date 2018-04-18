0cfabe16875d092cfbf5f91e641243f7f5d1ca68
Shubham Singh <shubham1998singh.ss@gmail.com>
2018-04-18 11:50:12 -0500

    Added uart

---
+++ uart_rx.sv
@@ -0,0 +1,109 @@
+//updated midnight
+/* Module for an 8-bit UART receiver.
+ * It takes clock, reset_n, input signal as inputs
+ * and outputs when the data is ready and the data.
+ *
+ */
+ module uart_rx(clk, rst_n, rx, rx_rdy, rx_data);
+
+	input clk, rst_n, rx;
+	output logic rx_rdy;
+	output reg [7:0] rx_data;
+	
+	reg [3:0] bit_cnt;		//counter to count total bits of data received
+	reg[11:0] baud_cnt;		//baud counter
+	
+	logic clr_shitf_reg, clr_baud, shitf;
+	logic half_baud, full_baud, bit_full;
+	
+	typedef enum reg[1:0] {IDLE, FRONT_PORCH, BACK_PORCH, RX} state_t; 
+	state_t state, next_state;
+	
+	assign half_baud = (baud_cnt == 1302) ? 1 : 0;
+	assign full_baud = (baud_cnt == 2604) ? 1 : 0;
+	assign bit_full = (bit_cnt == 4'h8) ? 1 : 0;
+
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			state <= IDLE;
+		else
+			state <=next_state;
+		
+	always_comb begin
+		//default values
+		clr_baud = 1;
+		clr_shitf_reg = 1;
+		shitf = 0;
+		next_state = IDLE;
+		rx_rdy = 0;
+		
+		case (state)
+			IDLE:
+				if (!rx) begin					//start bit detected
+					next_state = FRONT_PORCH;
+					clr_baud = 0;
+				end
+				
+			FRONT_PORCH:
+				if (half_baud)					//at middle of start bit
+					next_state = RX;
+				else begin
+					next_state = FRONT_PORCH;
+					clr_baud = 0;
+				end
+			
+			RX: 
+				if (!bit_full && full_baud) begin	//middle of a bit which isn't the stop bit
+					next_state = RX;
+					shitf = 1;
+					clr_shitf_reg = 0;
+				end
+				else if (bit_full) begin		//all bits received
+					next_state = BACK_PORCH;
+					clr_shitf_reg = 0;
+				end
+				else begin
+					next_state = RX;
+					clr_baud = 0;
+					clr_shitf_reg = 0;
+				end
+			
+			BACK_PORCH:
+				if (rx && half_baud) begin		//at middle of stop bit
+					next_state = IDLE;
+					rx_rdy = 1;
+				end
+				else if (half_baud)
+					next_state = IDLE;
+				else begin
+					next_state = BACK_PORCH;
+					clr_baud = 0;
+				end
+			endcase
+		end
+	
+	//data output register
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			rx_data <= 8'h00;
+		else if (shitf)
+			rx_data <= {rx, rx_data[7:1]};
+	
+	//baud counter
+	always_ff @(posedge clk, negedge rst_n)
+		if(!rst_n)
+			baud_cnt <= 12'h000;
+		else
+			baud_cnt <= clr_baud ? 0 : baud_cnt + 1;
+			
+	//bit counter
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			bit_cnt <= 4'h0;
+		else
+			if (clr_shitf_reg) 
+				bit_cnt <= 4'h0;
+			else if (full_baud)
+				bit_cnt <= bit_cnt + 1;
+
+endmodule				
---
+++ uart_tx.sv
@@ -0,0 +1,94 @@
+//updated midnight
+module uart_tx(clk, rst_n, tx_start, tx_data, tx, tx_rdy);
+
+  typedef enum reg [1:0] {IDLE, TX} state_t;
+  state_t state, nxt_state;
+
+  input clk, rst_n, tx_start;
+  input logic [7:0] tx_data;
+  output logic tx;
+  output logic tx_rdy;
+
+  logic bit_full, baud_full, clr, load, shift;
+  logic [11:0] baud;
+  logic [3:0] bit_cnt;
+  logic [9:0] shift_reg;
+
+  assign bit_full = (bit_cnt == 4'hA) ? 1 : 0;
+  assign baud_full = (baud == 12'hA2C) ? 1 : 0;
+  assign tx = shift_reg[0];
+
+//state machine
+always_ff @(posedge clk, negedge rst_n)
+    if (!rst_n)
+        state <= IDLE;
+    else
+        state <= nxt_state;
+
+always_comb begin
+  shift = 0;
+  load = 0;
+  clr = 0;
+  tx_rdy = 0;
+  //tx = 0;
+  nxt_state = IDLE;
+
+  case(state)
+    IDLE:
+      if (tx_start) begin
+        load = 1;
+        nxt_state = TX;
+    end
+    else
+      clr = 1;
+
+    TX:
+      if (baud_full)
+        if (!bit_full) begin
+          shift = 1;
+          clr = 1;
+          nxt_state = TX;
+			 //tx = shift_reg[0];
+        end
+        else begin
+          tx_rdy = 1;
+			 nxt_state = IDLE;
+			 //tx = shift_reg[0];
+			end
+      else begin
+        nxt_state = TX;
+		  //tx = shift_reg[0];
+		end
+  endcase
+end
+
+//baud counter
+always_ff @(posedge clk, negedge rst_n)
+  if (!rst_n)
+    baud <= 12'b0;
+  else
+    baud <= clr ? 12'b0 : baud + 1;
+
+//bit counter
+always_ff @(posedge clk, negedge rst_n)
+  if (!rst_n)
+    bit_cnt <= 4'b0;
+  else
+    if (load)
+      bit_cnt <= 4'b0;
+    else
+      if (shift)
+        bit_cnt <= bit_cnt + 1;
+
+//shift register
+always_ff @(posedge clk, negedge rst_n)
+  if (!rst_n)
+    shift_reg <= 0;
+  else
+    if (load)
+      shift_reg <= {1'b1, tx_data, 1'b0};
+    else
+      if (shift)
+        shift_reg <= {shift_reg[9], shift_reg[9:1]};
+
+endmodule
