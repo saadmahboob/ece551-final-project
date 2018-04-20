be2376ba1848b65f286a7298d23372b25ed5b358
Shubham Singh <shubham1998singh.ss@gmail.com>
2018-04-20 18:07:36 -0500

    Update

--- snn_core_tb.sv
+++ snn_core_tb.sv
@@ -1,1 +1,36 @@
+module snn_core_tb();
 
+logic clk, rst_n, rx_rdy, tx_start, strt, done, q_input, 
+logic []
+
+snn_core iDUT_snn_core(.strt(), .q_input(q_input), .addr_input_unit(), .digit(), .done());
+rom iDUT_ram(.data(), .addr(), .we(), .clk(), .q(q_input));
+
+task check_digit(input )
+
+
+module ram (
+    input data,
+    input [9:0] addr,
+    input we, clk,
+    output q);
+
+// Declare the RAM variable
+reg ram[2**9:0];
+
+// Variable to hold the registered read address
+reg [9:0] addr_reg;
+
+initial
+    readmemh("Initialization file", ram);
+end
+
+always @ (posedge clk) begin
+    if (we) // Write
+        ram[addr] <= data;
+        addr_reg <= addr;
+end
+
+assign q = ram[addr_reg];
+
+endmodule
