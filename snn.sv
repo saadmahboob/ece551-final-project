4ce3bd9b0cd0e3c87196b10c4cbe341ef198a5f4
Jon Sharp <jsharp4@wisc.edu>
2018-04-26 22:28:56 -0500

    stuff

--- snn.sv
+++ snn.sv
@@ -22,31 +22,70 @@ module snn(clk, sys_rst_n, led, uart_tx, uart_rx);
 	******************************************************/
 	
 	// Declare wires below
-	logic rx_rdy, tx_rdy, txrx;
-	logic[7:0] uart_data;
+	logic rx_rdy, tx_rdy, snn_done, q_input, ram_in;
+	logic[7:0] rx_data, tx_data;
+	logic[3:0] digit;
+	logic[9:0] addr_input_unit;
+
+	//control FSM wires
+	logic snn_start, write_enable, tx_rdy;
+
+	uart_rx uart_rx_in(.clk(clk), .rst_n(sys_rst_n), .rx(uart_rx_synch), .rx_data(rx_data), .rx_rdy(rx_rdy));
+	//logic between rx_in and tx_to_ram: rx_data, rx_rdy
+	uart_tx tx_to_ram(.clk(clk), .rst_n(sys_rst_n), .tx_start(rx_rdy), .tx_data(rx_data), .tx(ram_in), .tx_rdy(tx_rdy));
+	//logic between tx_to_ram and test_data: ram_in
+	ram test_data(.data(ram_in), .addr(addr_input_unit), .we(write_enable), .clk(clk), .q(q_input));
+	//logic between test_data and core: q_input. addr_input_unit
+	snn_core core(.start(snn_start), .q_input(q_input), .addr_input_unit(addr_input_unit), .digit(digit), .done(snn_done));
+	//logic between core and tx_out: snn_done, digit
+	uart_tx uart_tx_out(.clk(clk), .rst_n(sys_rst_n), .tx_start(snn_done), .tx_data(tx_data), .tx(uart_tx), .tx_rdy());
 	
 	// Double flop RX for meta-stability reasons
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n) begin
 		uart_rx_ff <= 1'b1;
 		uart_rx_synch <= 1'b1;
-	end else begin
+	end
+	 else begin
 	  uart_rx_ff <= uart_rx;
 	
 	uart_rx_synch <= uart_rx_ff;
 	end
 	
-	
+	assign tx_data = {4'h0, digit[3:0]};
+
+
 	// Instantiate UART_RX and UART_TX and connect them below
 	// For UART_RX, use "uart_rx_synch", which is synchronized, not "uart_rx".
-	 uart_rx uart_rx_DUT(.clk(clk), .rst_n(sys_rst_n), .rx(uart_rx_synch), .rx_data(uart_data), .rx_rdy(txrx));
-	 uart_tx uart_tx_DUT(.clk(clk), .rst_n(sys_rst_n), .tx_start(txrx), .tx_data(uart_data), .tx(uart_tx), .tx_rdy());
-     snn_core core(.start(), .q_input(), .addr_input_unit(), .digit(), .done());
-     ram test_data(.data(), .addr(addr_reg), .we(), .clk(clk), .q(q_input));
+
+	//control FSM
+	typedef enum reg[1:0] {IDLE, LOAD, READ} state_t;
+	state_t state, next_state;
+
+	always_ff (@posedge clk, negedge sys_rst_n) {
+		if (!rst_n)
+        	state <= IDLE;
+    	else
+    		state <= nxt_state;
+	}
+
+	always_comb begin
+		next_state = IDLE;
+		snn_start = 0;
+		write_enable = 0;
+
+		case(state)
+			IDLE:
+				if()
+
+
+     
+	 
+	 
 
 	/******************************************************
 	LED
 	******************************************************/
-	assign led = uart_data;
+	assign led = tx_data;
 
 endmodule
