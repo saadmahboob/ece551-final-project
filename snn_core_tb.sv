88c52cf452eb471d4351c6baccf0e3d83e4bd747
Shubham Singh <shubham1998singh.ss@gmail.com>
2018-05-02 11:44:33 -0500

    Added rst_synch

--- snn_core_tb.sv
+++ snn_core_tb.sv
@@ -51,7 +51,7 @@ reg ram[(2**10)-1:0];
 logic [9:0] addr_reg;
 
 initial begin
-    $readmemb("./Files/input\ samples/ram_input_contents_sample_4.txt", ram);
+    $readmemb("./Files/input\ samples/ram_input_contents_sample_0.txt", ram);
 
 end
 
