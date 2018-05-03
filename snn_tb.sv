d45f3b780c12b6cccb3022caac4c428b5b3fc10b
Jon Sharp <jsharp4@wisc.edu>
2018-05-03 14:42:17 -0500

    instantiated tx in tb

--- snn_tb.sv
+++ snn_tb.sv
@@ -4,33 +4,42 @@ localparam BAUD_PERIOD = 52100;
 
 logic clk, rst_n, tx_rx;
 logic[7:0] digit_out;
-logic tx_out;
-logic data_rdy;
-logic[7:0] data_in, led;
+logic snn_out;
+logic data_rdy, byte_rdy;
+logic[7:0] new_byte, led;
 logic[9:0] rom_addr;
 
-snn snn_DUT(.clk(clk), .sys_rst_n(rst_n), .led(led), .uart_tx(tx_out), .uart_rx(tx_rx));
-uart_rx rx_to_PC(.clk(clk), .rst_n(rst_n), .rx(tx_out), .rx_rdy(data_rdy), .rx_data(digit_out));
+snn snn_DUT(.clk(clk), .sys_rst_n(rst_n), .led(led), .uart_tx(snn_out), .uart_rx(tx_rx));
+uart_rx rx_to_PC(.clk(clk), .rst_n(rst_n), .rx(snn_out), .rx_rdy(data_rdy), .rx_data(digit_out));
 rom_tb rom_tb(.addr(rom_addr), .clk(clk), .q(q));
+uart_tx tx_to_snn(.clk(clk), .rst_n(rst_n), .tx_start(byte_rdy), .tx_data(new_byte), .tx(tx_rx), .tx_rdy());
 
 task test_snn;
     input logic[3:0] target_digit;
     input logic[7:0] target_led;
     int i, j;
+    byte_rdy = 0;
         for (i = 0; i < 98; ++i) begin
-            tx_rx = 0;                      //send start bit
-            #BAUD_PERIOD;                   //wait a baud period
+            //tx_rx = 0;                      //send start bit
+            //#BAUD_PERIOD;                   //wait a baud period
 
             for (j = 0; j < 8; ++j) begin
-                rom_addr = 8 * i + j;           //get new address to read from
-                @(posedge clk)              //wait a clock cycle for data to be valid
-                tx_rx = q;                  //send data
-                #(BAUD_PERIOD - 1);           //wait a baud period
+                new_byte[j] = rom_tb.rom[8 * i + j]; //get new address to read from
+            end
+            @(posedge clk)  
+            byte_rdy = 1;  
+            
+            @(posedge clk)
+            byte_rdy = 0;
+                
+                //@(posedge clk)              //wait a clock cycle for data to be valid
+                //tx_rx = q;                  //send data
+                //#(BAUD_PERIOD - 1);           //wait a baud period
             end
             
-            tx_rx <= 1'b1;                     //send stop bit
-            #BAUD_PERIOD;                   //wait baud period before going to next data
-        end
+            //tx_rx <= 1'b1;                     //send stop bit
+            //#BAUD_PERIOD;                   //wait baud period before going to next data
+        //end
 
     @(posedge data_rdy)
     if (digit_out[3:0] == target_digit)
@@ -53,9 +62,7 @@ initial begin
     rst_n = 0;
     clk = 0;
     tx_rx = 1;
-    data_in = 8'h00;
     rom_addr = 10'h0;
-
     #10;
     rst_n = 1;
     test_snn(4'h0, 8'h09);
