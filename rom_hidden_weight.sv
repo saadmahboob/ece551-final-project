93272b88f1a73e183e677f346fd45f4a8a7d707c
akshat97 <akshatkhanna97@gmail.com>
2018-05-04 02:41:30 -0500

    snn_core optimus changed

--- rom_hidden_weight.sv
+++ rom_hidden_weight.sv
@@ -1,12 +1,13 @@
 module rom_hidden_weight (
- input [14:0] addr,
+ input [14:0] addr1,
+ input [14:0] addr2,
  input clk,
  output reg [7:0] q);
  // Declare the ROM variable
  reg [7:0] rom[2**15-1:0];
  initial
     $readmemh("./Files/memory\ initialization\ files/rom_hidden_weight_contents.txt", rom);
- 
+
  always @ (posedge clk)
  begin
  q <= rom[addr];
--- snn_core.sv
+++ snn_core.sv
@@ -4,7 +4,7 @@ module snn_core(start, clk, rst_n, q_input, addr_input_unit, digit, done);
   state curr_state, next_state;
 
   output logic [9:0] addr_input_unit;
-  output logic [3:0] digit;
+  output logic [3:0] digit, digit1, digit2;
   output logic done;
 
   input logic clk, start, rst_n;
@@ -14,60 +14,100 @@ module snn_core(start, clk, rst_n, q_input, addr_input_unit, digit, done);
   logic mac_clr_n, output_clr, hidden_clr, addr_input_unit_clr, digit_clr;
 
   logic [7:0]   q_extended;
-  logic [7:0]   ram_hidden_data;
+  logic [7:0]   ram_hidden_data1, ram_hidden_data2;
   logic [5:0]   cnt_hidden;
-  logic [7:0]   hidden_weight_q;
+  logic [4:0]   cnt_hidden1, cnt_hidden2;
+  logic [7:0]   hidden_weight_q1, hidden_weight_q2;
 
   //MAC stuff
-  logic [7:0]   a, b, lut_out;
-  logic [25:0]  acc;
-  logic [10:0]  addr_LUT;
+  logic [7:0]   a, b1, b2, lut_out1, lut_out2;
+  logic [25:0]  acc1, acc2;
+  logic [10:0]  addr_LUT1, addr_LUT2;
 
   //Max stuff
-  logic [7:0]   output_weight_q, max_prob;
-  logic check_max;
-  logic [3:0]   digit_reg;
-  logic [3:0]   cnt_output;
+  logic [7:0]   output_weight_q, max_prob1, max_prob2;
+  logic         check_max;
+  logic [2:0]   digit_reg1;
+  logic [3:0]   digit_reg2;
+  logic [2:0]   cnt_output1, cnt_output2;
 
-  rom_hidden_weight    hidden_weight(.addr({cnt_hidden[4:0], addr_input_unit[9:0]}), .clk(clk), .q(hidden_weight_q));
-  rom_output_weight    output_weight(.addr({cnt_output[3:0], cnt_hidden[4:0]}), .clk(clk), .q(output_weight_q));
-  ram_hidden_unit      hidden_unit(.data(lut_out), .addr(cnt_hidden [4:0]), .clk(clk), .we(write_hidden), .q(ram_hidden_data));
-  rom_act_func_lut     act_func_lut(.addr(addr_LUT + 11'h400), .clk(clk), .q(lut_out));
-  mac                  mac(.clk(clk), .a(a), .b(b), .acc(acc), .clr_n(mac_clr_n), .rst_n(rst_n), .addr_LUT(addr_LUT));
+  rom_hidden_weight    hidden_weight(.clk(clk), .addr1({cnt_hidden1[4:0], addr_input_unit[9:0]}), .q1(hidden_weight_q1),
+                                                .addr2({cnt_hidden2[4:0], addr_input_unit[9:0]}), .q2(hidden_weight_q2));
+
+  rom_output_weight    output_weight(.clk(clk), .addr1({cnt_output1[3:0], cnt_hidden[4:0]}), .q1(output_weight_q1),
+                                                .addr2({cnt_output2[3:0], cnt_hidden[4:0]}), .q2(output_weight_q2));
+
+  ram_hidden_unit      hidden_unit(.clk(clk), .we(write_hidden), .data1(lut_out1), .addr1(cnt_hidden1 [4:0]), .q1(ram_hidden_data1),
+                                                                .data2(lut_out2), .addr2(cnt_hidden2 [4:0]), .q2(ram_hidden_data2));
+
+  rom_act_func_lut     act_func_lut(.clk(clk), .addr1(addr_LUT1 + 11'h400), .q1(lut_out1),
+                                                .addr2(addr_LUT2 + 11'h400), .q2(lut_out2));
+
+  mac                  mac1(.clk(clk), .a(a), .b(b1), .acc(acc1), .clr_n(mac_clr_n), .rst_n(rst_n), .addr_LUT(addr_LUT1));
+  mac                  mac2(.clk(clk), .a(a), .b(b2), .acc(acc2), .clr_n(mac_clr_n), .rst_n(rst_n), .addr_LUT(addr_LUT2));
 
   assign q_extended = (q_input) ? 8'h7F : 8'h00;
 
   //MAC 1
   assign addr_input_unit_max = addr_input_unit == 10'h2ec ? 1 : 0;
-  assign cnt_hidden_max = cnt_hidden == 6'h20 ? 1 : 0;
+  assign cnt_hidden_max = cnt_hidden1 == 5'h20 ? 1 : 0;
 
   //MAC 2
-  assign cnt_output_max =  (select_input) ? ((cnt_output == 4'ha) ? 1 : 0) : 0;
-  assign cnt_hidden_max_out = cnt_hidden == 6'h22 ? 1 : 0;
+  assign cnt_hidden_max_out = cnt_hidden == 6'h11 ? 1 : 0;
 
   assign a = select_input ? ram_hidden_data : q_extended;
-  assign b = select_input ? output_weight_q : hidden_weight_q;
-
+  assign b1 = select_input ? output_weight_q1 : hidden_weight_q1;
+  assign b2 = select_input ? output_weight_q2 : hidden_weight_q2;
 
  always_ff @(posedge clk, negedge rst_n)
   if (!rst_n)
-    max_prob <= 8'h00;
-  else 
+    max_prob1 <= 8'h00;
+  else
     if (digit_clr)
-      max_prob <= 8'h00;
+      max_prob1 <= 8'h00;
     else if (check_max)
-      if (lut_out > max_prob)
-        max_prob <= lut_out;
+      if (lut_out1 > max_prob1)
+        max_prob1 <= lut_out1;
 
   always_ff @(posedge clk, negedge rst_n)
    if (!rst_n)
-     digit <= 0;
+     max_prob2 <= 8'h00;
+   else
+     if (digit_clr)
+       max_prob2 <= 8'h00;
+     else if (check_max)
+       if (lut_out2 > max_prob2)
+         max_prob2 <= lut_out2;
+
+  always_ff @(posedge clk, negedge rst_n)
+   if (!rst_n)
+     digit1 <= 0;
   else
     if (digit_clr)
-      digit <= 0;
+      digit1 <= 0;
     else if (check_max)
-      if (lut_out > max_prob)
-       digit <= digit_reg;
+      if (lut_out1 > max_prob1)
+       digit1 <= digit_reg1;
+
+   always_ff @(posedge clk, negedge rst_n)
+    if (!rst_n)
+      digit2 <= 0;
+   else
+     if (digit_clr)
+       digit2 <= 0;
+     else if (check_max)
+       if (lut_out2 > max_prob2)
+        digit2 <= digit_reg2;
+
+    always_ff @(posedge clk, negedge rst_n)
+     if (!rst_n)
+       digit <= 0;
+    else
+      if (digit_clr)
+        digit <= 0;
+      else if (check_max)
+        if (digit1 > digit2)
+         digit <= digit1;
 
   // FSM
   always_ff @(posedge clk, negedge rst_n)
@@ -120,7 +160,7 @@ module snn_core(start, clk, rst_n, q_input, addr_input_unit, digit, done);
       MAC2: begin
         select_input = 1;
 
-        if (digit_reg == 4'ha)
+        if (digit_reg1 == 4'h5)
             next_state = DONE;
         else begin
           addr_input_unit_clr = 1;
@@ -164,16 +204,36 @@ module snn_core(start, clk, rst_n, q_input, addr_input_unit, digit, done);
   //hidden weight address counter
   always_ff @(posedge clk, negedge rst_n)
     if (!rst_n)
-      cnt_hidden <= 6'b0;
+      cnt_hidden1 <= 6'b0;
     else
       if (hidden_clr)
-        cnt_hidden <= 6'b0;
-      else if (cnt_hidden_max_out)
-        cnt_hidden <=6'b0;
+        cnt_hidden1 <= 6'b0;
       else if (addr_input_unit_max)
-        cnt_hidden <= cnt_hidden + 1;
+        cnt_hidden1 <= cnt_hidden1 + 1;
       else if (increment_output)
-        cnt_hidden <= cnt_hidden + 1;
+        cnt_hidden1 <= cnt_hidden1 + 1;
+
+    always_ff @(posedge clk, negedge rst_n)
+      if (!rst_n)
+        cnt_hidden2 <= 6'b10;
+      else
+        if (hidden_clr)
+          cnt_hidden2 <= 6'b10;
+        else if (addr_input_unit_max)
+          cnt_hidden2 <= cnt_hidden2 + 1;
+        else if (increment_output)
+          cnt_hidden2 <= cnt_hidden2 + 1;
+
+    always_ff @(posedge clk, negedge rst_n)
+      if (!rst_n)
+        cnt_hidden <= 6'b0;
+      else
+        if (hidden_clr)
+          cnt_hidden <= 6'b0;
+        else if (addr_input_unit_max)
+          cnt_hidden <= cnt_hidden + 1;
+        else if (increment_output)
+          cnt_hidden <= cnt_hidden + 1;
 
   //output weight address counter
   always_ff @(posedge clk, negedge rst_n)
@@ -189,12 +249,22 @@ module snn_core(start, clk, rst_n, q_input, addr_input_unit, digit, done);
   //digit of max prob needs to be one clock cycle behind input address
   always_ff @(posedge clk, negedge rst_n)
     if (!rst_n)
-      digit_reg <= 4'h0;
+      digit_reg1 <= 4'h0;
     else
       if (digit_clr)
-        digit_reg <= 4'h0;
+        digit_reg1 <= 4'h0;
       else
         if (check_max)
-          digit_reg <= digit_reg + 1;
+          digit_reg1 <= digit_reg1 + 1;
+
+  always_ff @(posedge clk, negedge rst_n)
+    if (!rst_n)
+      digit_reg2 <= 4'h5;
+    else
+      if (digit_clr)
+        digit_reg2 <= 4'h5;
+      else
+        if (check_max)
+          digit_reg2 <= digit_reg2 + 1;
 
 endmodule
